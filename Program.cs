6ff9b26348bcb0ccab2295da59dab9b55417082c
Sravya Sunkara <sravyayadav678@gmail.com>
2025-11-10 10:59:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace two;
+public class two()
+{
+    public static void Main(string[] args)
+    {
+        //revsersal of name
+        //string name = "John K";
+        //string outp = new string(name.Reverse().ToArray());
+        //Console.WriteLine(outp);
+        //exec problem
+        string fullname = "Sravya Sunkara Yadav";
+
+        string[] splitname = fullname.Split(' ');
+        Array.Reverse(splitname);
+        string revname = string.Join(" ", splitname);
+        Console.WriteLine(revname);
+
+
+
+    }
+
+}
